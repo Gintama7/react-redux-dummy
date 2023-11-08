e757db769f84528c7db95423d7b8d23cf037ab06
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-11-08 15:08:12 +0530

    subscribing and dispatching actions

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,13 @@ import { createStore } from "redux";
 
 
 const counterReducer=(state={counter:0},action)=>{
-    if(action.type === 'incrementby2')
+    if(action.type === 'increment')
     {
-        return {counter: state.counter+2}
+        return {counter: state.counter+5}
     }
-    if(action.type === 'decrementby2')
+    if(action.type === 'decrement')
     {
-        return {counter: state.counter-2}
+        return {counter: state.counter-5}
     }
     return state;
 }
